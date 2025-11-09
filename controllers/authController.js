5ee09e5bbc585c202fb056a1fad37fed5082671a
Nugie kurniawan <nugiebinhar@gmail.com>
2025-11-09 16:00:34 +0700

    register:berhasil menambahkan data tetapi belum validasi

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,23 +1,21 @@
 import {Users} from "../models/User.model.js";
 import { hash } from "bcrypt";
 export const register = async (req, res) => {
-    // try {
-    //     const {nama, email, password, confirmPass} = req.body;
-    //     console.log(req.body);
-    //     if (password !== confirmPass) {
-    //        throw new Error("password dan konfirmasi password tidak sama");
-    //     }
-    //     const hashedPass = await hash(password,10);
-    //     // tambahkan
-    //     const data = {
-    //         name:nama,
-    //         email,
-    //         password:hashedPass,
-    //     };
-    //     const tambah = await Users.create({data});
-    //     return res.status(200).json({message: "berhasil menambahkan data"});
-    // } catch (error) {
-    //     return res.status(400).json({message:error.message})
-    // }
-    res.json(req.body)
+    try {
+        const {nama, email, password, confirmPass} = req.body;
+        if (password !== confirmPass) {
+           throw new Error("password dan konfirmasi password tidak sama");
+        }
+        const hashedPass = await hash(password,10);
+        // tambahkan
+        const data = {
+            name:nama,
+            email,
+            password:hashedPass,
+        };
+        const tambah = await Users.create({data});
+        return res.status(200).json({message: "berhasil menambahkan data"});
+    } catch (error) {
+        return res.status(400).json({message:error.message})
+    }
 };
