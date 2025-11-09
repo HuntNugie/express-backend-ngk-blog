c4a558a14500412796d1b05f16830919881b53ee
Nugie kurniawan <nugiebinhar@gmail.com>
2025-11-09 15:41:26 +0700

    masih error

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,23 @@
-export const register = (req,res)=>{
-    const {nama,email,password,confirmPass} = req.body;
-    console.log(req.body)
-    res.status(200).json(req.body)
-}
+import {Users} from "../models/User.model.js";
+import { hash } from "bcrypt";
+export const register = async (req, res) => {
+    // try {
+    //     const {nama, email, password, confirmPass} = req.body;
+    //     console.log(req.body);
+    //     if (password !== confirmPass) {
+    //        throw new Error("password dan konfirmasi password tidak sama");
+    //     }
+    //     const hashedPass = await hash(password,10);
+    //     // tambahkan
+    //     const data = {
+    //         name:nama,
+    //         email,
+    //         password:hashedPass,
+    //     };
+    //     const tambah = await Users.create({data});
+    //     return res.status(200).json({message: "berhasil menambahkan data"});
+    // } catch (error) {
+    //     return res.status(400).json({message:error.message})
+    // }
+    res.json(req.body)
+};
