3b55d8ababc530e28ff96de55717447873a6140c
Nugie kurniawan <nugiebinhar@gmail.com>
2025-11-11 23:59:25 +0700

    login:setup untuk fitur login

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,21 +1,43 @@
 import {Users} from "../models/User.model.js";
-import { hash } from "bcrypt";
+import {hash,compare} from "bcrypt";
 export const register = async (req, res) => {
     try {
         const {nama, email, password, confirmPass} = req.body;
         if (password !== confirmPass) {
-           throw new Error("password dan konfirmasi password tidak sama");
+            throw new Error("password dan konfirmasi password tidak sama");
         }
-        const hashedPass = await hash(password,10);
+        const hashedPass = await hash(password, 10);
         // tambahkan
         const data = {
-            name:nama,
+            name: nama,
             email,
-            password:hashedPass,
+            password: hashedPass,
         };
         const tambah = await Users.create({data});
         return res.status(200).json({message: "berhasil menambahkan data"});
     } catch (error) {
-        return res.status(400).json({message:error.message})
+        return res.status(400).json({message: error.message});
     }
+};
+
+export const login = async (req, res) => {
+    try {
+        const {email, password} = req.body;
+        const result = await Users.findMany({
+            where: {email: email},
+        });
+        
+        // cek data
+        if(!result){
+            return res.status(404).json({
+                message:"anda belum pernah login"
+            });
+        }
+
+        // cek apakah password yang ada di user itu benar
+        const hasil = await compare(password,result.password);
+        if(!hasil){
+            return res.status.json({message:"username atau password salah"});
+        }
+    } catch (error) {}
 };
