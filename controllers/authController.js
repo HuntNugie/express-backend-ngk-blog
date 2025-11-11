414b3e66cc1a8fe35c80f6e1a96f5c772dadc43c
Nugie kurniawan <nugiebinhar@gmail.com>
2025-11-12 00:15:07 +0700

    login:setup final

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,7 @@
 import {Users} from "../models/User.model.js";
 import {hash,compare} from "bcrypt";
+import jwt from "jsonwebtoken";
+import {config} from "dotenv";
 export const register = async (req, res) => {
     try {
         const {nama, email, password, confirmPass} = req.body;
@@ -37,7 +39,22 @@ export const login = async (req, res) => {
         // cek apakah password yang ada di user itu benar
         const hasil = await compare(password,result.password);
         if(!hasil){
-            return res.status.json({message:"username atau password salah"});
+            return res.status(401).json({message:"username atau password salah"});
         }
-    } catch (error) {}
+
+        // buat token sudah beres
+        const token = jwt.sign({
+            "user_id" : result.id
+        },process.env.SECRET_JWT,{expiresIn:"10m"});
+
+        res.cookie("token",token,{
+            httpOnly:true,
+            secure:true,
+            sameSite:"none"
+        });
+        return res.status(200).json({message:"berhasil login"});
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({message:"ada masalah : "+error});
+    }
 };
