dd8d0dbbf29d126d35611b6413221d4a9e72d251
Nugie kurniawan <nugiebinhar@gmail.com>
2025-11-14 20:27:10 +0700

    middleware:membuat middleware untuk auth

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,15 @@
+import {verifikasiToken} from "../utils/jwt.js";
+export const auth = async (req, res, next) => {
+    try {
+        const token = req.cookies?.token;
+        if (!token) {
+            return res.status(401).json({message: "token tidak ada", status: false});
+        }
+
+        const verif = await verifikasiToken(token);
+        req.auth = verif;
+        next();
+    } catch (error) {
+        res.status(401).json({message: "token expired", status: false});
+    }
+};
